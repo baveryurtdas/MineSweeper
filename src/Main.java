f801049907656608a6843800252940df0af47e09
Baver Yurtdas <baveryurtdas@gmail.com>
2023-10-21 19:55:43 +0300

    mayin tarlasi oyunu eklnedi

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        MineSweeper mineSweeper = new MineSweeper();
+        mineSweeper.run();
+    }
+}
